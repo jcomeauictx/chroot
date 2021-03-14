cb3f61fa97524ce25e851560c101516285f4e1fa
John Comeau <jc@unternet.net>
2021-03-14 22:37:22 +0000

    new project. I keep having to re-develop this every time I change computers.

---
+++ chroot.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -x
+chrootdir="${1:-/mnt/new/opt/jessie32}"
+dirs="proc /sys /dev /home /usr/src /mnt/*"
+files="/etc/mtab /etc/passwd /etc/group /etc/shadow"
+for dir in $dirs $files; do
+	if [ "${dir:0:1}" = "/" ]; then
+		mount --bind $dir $chrootdir$dir
+	elif [ "$dir" = "/sys" ]; then
+		mount -t sysfs sysfs $dir
+	else
+		mount -t $dir $dir $chrootdir/$dir
+	fi
+done
+chroot $chrootdir
+for dir in $(echo $dirs $files | tr " " "\n" | tac); do
+	if [ "${dir:0:1}" = "/" ]; then
+		umount $chrootdir$dir
+	else
+		umount $chrootdir/$dir
+	fi
+done
