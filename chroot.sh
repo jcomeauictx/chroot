7b160e502f3edf03cc7fc863f6ee82e4ce3c279c
John Comeau <jc@unternet.net>
2023-11-11 17:13:19 -0700

    cleanup, but doesn't actually work

--- chroot.sh
+++ chroot.sh
@@ -1,21 +1,11 @@
 #!/bin/bash -x
-chrootdir="${1:-/mnt/new/opt/jessie32}"
-dirs="proc /sys /dev /home /usr/src /mnt/*"
+chrootdir="${1:-/opt/stretch32}"
+dirs="/proc /sys /dev /home /usr/src /mnt"
 files="/etc/mtab"
 for dir in $dirs $files; do
-	if [ "${dir:0:1}" = "/" ]; then
-		mount --bind $dir $chrootdir$dir
-	elif [ "$dir" = "/sys" ]; then
-		mount -t sysfs sysfs $dir
-	else
-		mount -t $dir $dir $chrootdir/$dir
-	fi
+	sudo mount --bind $dir $chrootdir$dir
 done
-chroot $chrootdir
+sudo chroot $chrootdir
 for dir in $(echo $dirs $files | tr " " "\n" | tac); do
-	if [ "${dir:0:1}" = "/" ]; then
-		umount $chrootdir$dir
-	else
-		umount $chrootdir/$dir
-	fi
+	sudo umount $chrootdir$dir
 done
