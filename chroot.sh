70c1dba67b4eeba2ab70fffba6ad13fff11b8152
John Comeau <jc@unternet.net>
2024-01-28 19:25:52 -0800

    added improvement from rentacoder project

--- chroot.sh
+++ chroot.sh
@@ -1,11 +1,12 @@
 #!/bin/bash -x
 chrootdir="${1:-/opt/stretch32}"
+username="${2:-$USER}"
 dirs="/proc /sys /dev /home /usr/src /mnt"
 files="/etc/mtab"
 for dir in $dirs $files; do
 	sudo mount --bind $dir $chrootdir$dir
 done
-sudo chroot $chrootdir
+sudo chroot $chrootdir /bin/bash -c "su --login $username"
 for dir in $(echo $dirs $files | tr " " "\n" | tac); do
 	sudo umount $chrootdir$dir
 done
